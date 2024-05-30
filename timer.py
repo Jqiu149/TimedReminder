42d36953b4d0d0e7a0b703f082d968fb6cb68ccc
jasonqiu93 <jasonqiu93@penguin>
2024-05-30 13:42:59 -0700

    replaced use of thread.Timer with time.sleep()

--- timer.py
+++ timer.py
@@ -1,17 +1,19 @@
 from tkinter import *
 from tkinter import ttk
-import threading
+import time
+
+TIMER_LENGTH = 60 * 20; 
+DEFAULT_BREAK_MESSAGE = "take a small break for eyes at aleast. maybe a longer break if been working for a bit or stressed"
 
 def setTimer():
-    my_timer = threading.Timer(1200, CreatePopUpReminder)
-    my_timer.start()
+    time.sleep(TIMER_LENGTH)
 
 
 def CreatePopUpReminder():
     root = Tk()
     frm = ttk.Frame(root, padding=10)
     frm.grid()
-    ttk.Label(frm, text="5 min break >:(((( !").grid(column=0, row=0)
+    ttk.Label(frm, text = DEFAULT_BREAK_MESSAGE).grid(column=0, row=0)
     ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
     ttk.Button(frm, text="again",command=lambda:[setTimer(),root.destroy()]).grid(column=2, row=0)    
     root.mainloop()
@@ -19,4 +21,6 @@ def CreatePopUpReminder():
 
 my_timer = threading.Timer(1200, CreatePopUpReminder)
 my_timer.start()
+
+
 
