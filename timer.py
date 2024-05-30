6bff0288700b5d64cccb1af231bc4283249ea95c
jasonqiu93 <jasonqiu93@penguin>
2024-05-30 13:52:07 -0700

    we didn't do testing last time... should probably do that next timer before commiting huh.... but issue occured. calling setTimer before destroying the pop up caused sleep to run before we destroyed our thing... and it's not a thread anymore so not happening seperately ig?

--- timer.py
+++ timer.py
@@ -2,11 +2,12 @@ from tkinter import *
 from tkinter import ttk
 import time
 
-TIMER_LENGTH = 60 * 20; 
+TIMER_LENGTH = 20; 
 DEFAULT_BREAK_MESSAGE = "take a small break for eyes at aleast. maybe a longer break if been working for a bit or stressed"
 
 def setTimer():
     time.sleep(TIMER_LENGTH)
+    CreatePopUpReminder()
 
 
 def CreatePopUpReminder():
@@ -15,12 +16,9 @@ def CreatePopUpReminder():
     frm.grid()
     ttk.Label(frm, text = DEFAULT_BREAK_MESSAGE).grid(column=0, row=0)
     ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-    ttk.Button(frm, text="again",command=lambda:[setTimer(),root.destroy()]).grid(column=2, row=0)    
+    ttk.Button(frm, text="again",command=lambda:[root.destroy(),setTimer()]).grid(column=2, row=0)    
     root.mainloop()
 
 
-my_timer = threading.Timer(1200, CreatePopUpReminder)
-my_timer.start()
-
-
+setTimer(); 
 
