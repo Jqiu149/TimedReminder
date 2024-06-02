0c9933772c7f7722c93daa97ab8e8ccc50e750e6
jasonqiu93 <jasonqiu93@penguin>
2024-06-01 21:41:05 -0700

    added more messages... i'm normal i swear

--- timer.py
+++ timer.py
@@ -4,19 +4,21 @@ import time
 import random
 
 
-TIMER_LENGTH = 20 * 60; # in seconds 
+TIMER_LENGTH = 1; # in seconds 
 DEFAULT_BREAK_MESSAGE = "20 sec break for eyes. rah >:( .... or longer break if tired/stress."
 break_message = DEFAULT_BREAK_MESSAGE; 
 
 messages = [
     "you're doing great >~<",
-    "slaying !!!!!", 
-    "ineedadrinkimsocringeholyshitwtfisthisidkman", 
+    "you're slaying !!!!!", 
+    "hey, you're super cool btw (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧", 
     "\"postive message\"",
     "I apreciate you a lot honey ;---;",
     "thank you for trying your best !!!",
     "尽管你一再让我心碎，我想告诉你，如果有来生，我还是会选择和你一起，报税，开洗衣店",
-
+    "you make me want to be a better man ;-----;",
+    "thank you for being alive and waking up today.",
+    "woah you're kinda cute (⁄ ⁄•⁄ω⁄•⁄ ⁄)"
 ]
 
 def setMessage(message):
@@ -48,7 +50,7 @@ def CreatePopUpReminder():
     #hey REMMEBER TH EORDER OF THE FUCNTIONS PASSED IN HERE MATTER
 
     if(random.randint(0,20) == 7):
-        ttk.Label(frame, text = random.choice(messages)).grid(column =0, row = 4)
+        ttk.Label(frame, text = random.choice(messages)).grid(column =0, row = 3, pady = (40, 0))
 
     root.mainloop()
 
