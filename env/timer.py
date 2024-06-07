f7897be776de2711b4accf5bd083293a1ba99e4b
jasonqiu93 <jasonqiu93@penguin>
2024-06-06 17:17:11 -0700

    changed code so it reads frome the user_settings.json file and uses values stored there for stuff. also, if starting userID is null then it should get the next availible id and overwrite the file replacing the null value with that id. from here i think we should be able to do settings stuff

--- env/timer.py
+++ env/timer.py
@@ -3,53 +3,37 @@ from tkinter import ttk
 import time
 import random
 import os
-
+import json
 from supabase import create_client, Client
-
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
-TIMER_LENGTH = 20*60; # in seconds 
-DEFAULT_BREAK_MESSAGE = "20 sec break for eyes. rah >:( .... or longer break if tired/stress."
-break_message = DEFAULT_BREAK_MESSAGE; 
-
 MAX_VAL_SINT_4BYTES = (2**30) - 1
 
-messages = [
-    "you're doing great >~<",
-    "you're slaying !!!!!", 
-    "hey, you're super cool btw (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧", 
-    "\"postive message\"",
-    "I apreciate you a lot honey ;---;",
-    "thank you for trying your best !!!",
-    "尽管你一再让我心碎，我想告诉你，如果有来生，我还是会选择和你一起，报税，开洗衣店 ;-----------; hhhhhhhh",
-    "you make me want to be a better man ;-----;",
-    "thank you for being alive and waking up today.",
-    "woah you're kinda cute (⁄ ⁄•⁄ω⁄•⁄ ⁄)"
-]
-
-def setMessage(message):
+def set_break_message(message):
     if(message != ""):
         global break_message
         break_message = message
 
 def setTimer():
-    time.sleep(TIMER_LENGTH)
+    time.sleep(user_settings["default_timer_length"])
     CreatePopUpReminder()
 
 def storeData(startTime, endTime):
-    time_elapsed = int(endTime - startTime)
-    user_id = 1; 
+    print("hey commented the data storing out for now, uncomment later")
+    # time_elapsed = int(endTime - startTime)
+    # user_id = user_settings["user_id"]; 
 
-    if(time_elapsed > MAX_VAL_SINT_4BYTES or time_elapsed < 0):
-        time_elapsed = None
-    supabase.table('userData').insert({"userId": user_id, "time_elapsed_seconds": time_elapsed}).execute(); 
+    # if(time_elapsed > MAX_VAL_SINT_4BYTES or time_elapsed < 0):
+    #     time_elapsed = None
+    # supabase.table('userData').insert({"userId": user_id, "time_elapsed_seconds": time_elapsed}).execute(); 
 
 def CreatePopUpReminder():
-    global break_message, timer_start_time
-
+    global break_message
+    
+    timer_start_time = time.time()
     root = Tk() #initializes tcl/tk interpreter thingy? is the gui interface
                 #also creates root window? 
     
@@ -63,24 +47,36 @@ def CreatePopUpReminder():
     frame.grid()
     
     ttk.Label(frame, text = break_message).grid(column =0, row = 0)
-    break_message = DEFAULT_BREAK_MESSAGE
+    break_message = user_settings["default_break_message"]
 
     ttk.Label(frame, text = "enter a custom message for next popUp if you want").grid(column = 0, row = 1)
     
     entryBox = ttk.Entry(frame)
     entryBox.grid(column = 1, row = 1)
 
-    ttk.Button(frame, text="again",command=lambda:[storeData(timer_start_time, time.time()), setMessage(entryBox.get()), root.destroy(),setTimer()]).grid(column=3, row=1)    
-    #hey REMMEBER TH EORDER OF THE FUCNTIONS PASSED IN HERE MATTER
+    ttk.Button(frame, text="again",command=lambda:[storeData(timer_start_time, time.time()), set_break_message(entryBox.get()), root.destroy(),setTimer()]).grid(column=3, row=1)    
+    #hey REMMEBER THE ORDER OF THE FUCNTIONS PASSED IN HERE MATTER
 
     ttk.Button(frame, text="quit",command= lambda:[storeData(timer_start_time, time.time()),root.destroy()]).grid(column=4, row=1)    
 
     if(random.randint(0,20) == 7):
-        ttk.Label(frame, text = random.choice(messages)).grid(column =0, row = 3, pady = (40, 0))
+        ttk.Label(frame, text = random.choice(user_settings["extra_messages"])).grid(column =0, row = 3, pady = (40, 0))
 
-    timer_start_time= time.time()
     root.mainloop()
 
-timer_start_time = 0
+
+#initialize user settings 
+user_settings_file = open('user_settings.json',"r")
+user_settings = json.load(user_settings_file)
+user_settings_file.close()
+
+if(user_settings["user_id"] is None):
+    user_settings_file = open('user_settings.json',"w")
+    user_settings["user_id"] = supabase.table("user_id").select("*").execute().data[0]["next_user_id"]
+    supabase.table("user_id").update({'next_user_id': user_settings["user_id"] + 1}).eq('next_user_id', user_settings["user_id"] ).execute()
+    json.dump(user_settings, user_settings_file) #seems like not happenign until program ends? or maybe 
+    user_settings_file.close()
+
+break_message = user_settings["default_break_message"]
 setTimer(); 
 
